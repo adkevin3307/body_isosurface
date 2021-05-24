6c345865ecef6fd5a16b93e70e5aa0aea1cfbcbe
adkevin3307 <adkevin3307@gmail.com>
2021-05-24 14:06:23 +0800

    add iso_surface_wrapper

---
+++ iso_surface_wrapper.cpp
@@ -0,0 +1,30 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <sstream>
+
+#include "Volume.h"
+#include "IsoSurface.h"
+
+using namespace std;
+
+PYBIND11_MODULE(_iso_surface, m)
+{
+    pybind11::class_<IsoSurface>(m, "IsoSurface")
+        .def(pybind11::init<>())
+        .def(pybind11::init<Volume>())
+        .def("__str__", [](IsoSurface& m) {
+            stringstream ss;
+            ss << m;
+
+            return ss.str();
+        })
+        .def("run", &IsoSurface::run)
+        .def_property("iso_value", [](IsoSurface& iso_surface) {
+            return iso_surface.iso_value();
+        }, [](IsoSurface& iso_surface, float value) {
+            iso_surface.iso_value() = value;
+        })
+        .def_property_readonly("shape", &IsoSurface::shape)
+        .def_property_readonly("vertices", &IsoSurface::vertices)
+        .def_property_readonly("normals", &IsoSurface::normals);
+}
